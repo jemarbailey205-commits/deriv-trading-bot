7f9a56dc31690b9876754c4f76b9fd3c2c88b7c1
jemarbailey205-commits <jemarbailey205@gmail.com>
2026-09-10 22:36:21 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ import pandas as pd
 import ta
 
 # ====== CONFIG ======
-API_TOKEN = os.environ.get("DERIV_API_TOKEN", "pat_3d4176dca9da8e3eeebbc2ef303e91725c0005a721f675e5287acfb67245a953)
+API_TOKEN = os.environ.get("DERIV_API_TOKEN")
 APP_ID = 1089  # Deriv's default public app_id, fine for personal bots
 SYMBOL = "R_75"          # e.g. Volatility 75 Index. Change to "frxEURUSD" etc for forex
 GRANULARITY = 3600        # candle size in seconds (3600 = 1 hour, 86400 = 1 day)
